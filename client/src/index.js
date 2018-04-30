35fbb88109694998823e55c2d2601e5a0e7544c0
Charlotte Cady <charlottecady@MacBook-Pro-de-Charlotte.local>
2018-04-30 15:26:50 +1000

    add sw

--- client/src/index.js
+++ client/src/index.js
@@ -2,25 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
-
-// import React from 'react';
-// import { hydrate, render } from 'react-dom';
-// import './index.css';
-// import App from './components/App';
-// import registerServiceWorker from './registerServiceWorker';
-// import { unregister } from './registerServiceWorker';
-//
-// const rootElement = document.getElementById('root');
-// if (rootElement.hasChildNodes()) {
-//   hydrate(<App />, rootElement);
-//   unregister()
-//   registerServiceWorker();
-// } else {
-//   render(<App />, rootElement);
-//   unregister()
-//   registerServiceWorker();
-// }
+// registerServiceWorker();
